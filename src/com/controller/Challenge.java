975d89d2f6c3839ca696c7b7f321747a300f7c82
Eduardo Rivas <ineverdie07@gmail.com>
2022-09-20 19:49:00 -0500

    Span selection proccess. Challenge resolved.

--- src/com/controller/Challenge.java
+++ src/com/controller/Challenge.java
@@ -1,9 +1,21 @@
 package com.controller;
 
+import java.util.Scanner;
+
+import com.exceptions.GameException;
+
 public class Challenge {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner readConsole = new Scanner(System.in);
+		
+		try {
+			System.out.println(Game.readInput(readConsole.nextLine()));
+		} catch (GameException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
